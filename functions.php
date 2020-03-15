4497064b938bcc44d04c00d414feff89398a94da
Rustam <punk60826@gmail.com>
2020-03-15 23:45:39 +0200

    campuses done

--- functions.php
+++ functions.php
@@ -31,6 +31,7 @@ function pageBanner($args = NULL) {
 <?php }
 
 function university_files() {
+  wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyCQdsXyKjiDJbyU8m09LYfhT0cgqQafjL8', NULL, '1.0', true);
   wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
@@ -55,6 +56,10 @@ function university_adjust_queries($query) {
     $query->set('order', 'ASC');
     $query->set('posts_per_page', -1);
   }
+  if (!is_admin() AND is_post_type_archive('campus') AND is_main_query()) {
+    $query->set('posts_per_page', -1);
+  }
+
 
   if (!is_admin() AND is_post_type_archive('event') AND is_main_query()) {
     $today = date('Ymd');
