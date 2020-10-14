cf93db11a4c10505a42c69a4689c742b27712796
Rustam <punk60826@gmail.com>
2020-10-14 21:59:16 +0300

    Search.js file

---
+++ js/modules/Search.js
@@ -0,0 +1,7 @@
+class Search {
+    constructor(){
+        alert("Hello, I'm from search."); 
+    }
+}
+
+export default Search;
--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,10 @@ import slick from 'slick-carousel';
 import MobileMenu from './modules/MobileMenu';
 import HeroSlider from './modules/HeroSlider';
 import GoogleMap from './modules/GoogleMap';
+import Search from './modules/Search';
 
 var mobileMenu = new MobileMenu();
 var heroSlider = new HeroSlider();
 var googleMap = new GoogleMap();
+var search = new Search();
+         
