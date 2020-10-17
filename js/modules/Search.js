21e202e2b84bb9b9d9e18d0c60000ff7a906d27b
Rustam <punk60826@gmail.com>
2020-10-18 01:08:17 +0300

    localize js files and ternary operator for checking is empty or not in search

--- js/modules/Search.js
+++ js/modules/Search.js
@@ -39,8 +39,20 @@ class Search {
       }
 
       getResults(){
-        this.resultsDiv.html("Searching...");
-        this.isSpinnerVisible = false;
+          $.getJSON(universityData.root_url +'/wp-json/wp/v2/posts?search=' + this.searchField.val(), posts => {
+              this.resultsDiv.html(`
+              <h2 class="search-overlay__section-title">General Information</h2>
+
+              ${posts.length ? '<ul class="link-list min-list">' : '<p>No general information</p>'}
+
+              ${posts.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')}
+
+              ${posts.length ? '</ul>' : ''}
+       
+            `);
+
+            this.isSpinnerVisible = false;
+          });      
     }
 
 
